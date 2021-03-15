cd8af61e7efeef98133ac22b67d81d804bd89b88
Stanley Dave Teherag <sdaveteherag@gmail.com>
2021-03-15 18:36:24 +0700

    22 February 2021 - 3

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   endpoint: 'https://tpa-web-st.herokuapp.com/query',
-  websocket: 'ws://tpa-web-st.herokuapp.com/query',
+  websocket: 'wss://tpa-web-st.herokuapp.com/query',
 };
